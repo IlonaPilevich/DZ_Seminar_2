726dc39f764fe63f574a550df99fb932ca26d6eb
Ilona <ppsport88@mail.ru>
2023-02-15 19:14:15 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите день недели цифрой: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 5)
+{
+    Console.WriteLine(number + " - этот день недели НЕ выходной ");
+}
+if (number > 5)
+{
+    Console.WriteLine(number + " - этот день недели является выходным ");
+}
