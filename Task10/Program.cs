d38619c18386dbb74d2111b71ff29d17f7b10373
Ilona <ppsport88@mail.ru>
2023-02-15 18:34:23 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = (number / 10 % 10);
+
+Console.WriteLine("Исходное число: " + number + ", вторая цифра числа " + result);
